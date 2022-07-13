c279cc9157cfdd686fba7cde1c374016a092f3ca
LuisNietoM <luis.nieto@updigital.ac.pa>
2022-07-13 12:37:33 -0500

    primera compra

--- funciones/agregar-carrito.php
+++ funciones/agregar-carrito.php
@@ -11,3 +11,16 @@ try {
 } catch (\Throwable $th) {
     header("location: ../carrito.php");
 }
+
+# Actualiar las existencias
+if ($_GET['id']) {
+    $id = $_GET['id'];
+    $sql = "SELECT existencia FROM productos WHERE id=$id;";
+    $result = $base_de_datos->query($sql, PDO::FETCH_ASSOC)->fetchAll();
+    foreach ($result as $value) {
+        $existencia = $value['existencia'];
+    }
+    $existencia = $existencia - 1;
+    $sql = "UPDATE `productos` SET `existencia` = $existencia WHERE `productos`.`id` = $id;";
+    $base_de_datos->query($sql);
+}
